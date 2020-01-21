07436268951b7947fed9313c2d0e3b8e71ab4427
CarlosOrdonez <54687569+CarlosOrdonez@users.noreply.github.com>
2020-01-21 14:12:58 -0800

    Add files via upload

---
+++ Drowning.rb
@@ -0,0 +1,91 @@
+use_bpm 129
+play :e4
+play :g4
+play:c3
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+play :c3
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :a4
+play :a3
+sleep 0.5
+play :c4
+sleep 0.5
+play :e4
+play :g4
+play :g3
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+play :b2
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+sleep 0.5
+play :c4
+sleep 0.5
+play :a4
+play :d4
+play :a3
+sleep 0.5
+play :c4
+sleep 0.5
+play :g4
+play :d4
+play :g3
+sleep 0.5
+play :c4
+sleep 0.5
+
+
+
+
+
+
+
+
